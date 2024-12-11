751586d43006c5f95940cbf3dbdffc3c4d4569d5
Johnny Gérard <john@jgerard.dev>
2024-12-11 23:46:16 +0100

    build: downgrade to Angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import {
-  provideClientHydration,
-  withEventReplay,
-} from "@angular/platform-browser";
+import { provideClientHydration } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
@@ -11,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(),
   ],
 };
--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-not-found-page",
+  standalone: true,
   imports: [RouterLink],
   templateUrl: "./not-found-page.component.html",
   host: { class: "block" },
